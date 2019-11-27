ec12e38f6ffe89a390a7d98f7128d6672f2df1c7
duzi55 <1375744701@qq.com>
2019-11-27 19:07:26 +0800

    mybook

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndextapsComponent } from './compoent/indextaps/indextaps.component';
+import { BookdetailsComponent } from './compoent/bookdetails/bookdetails.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index', component: IndextapsComponent
+  },
+  {
+    path: 'bookdetails', component: BookdetailsComponent,
+  },
+  {
+    path: '', component: IndextapsComponent,
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './nav/header/header.component';
+import { EditorComponent } from './editor/editor.component';
+import { CarouselComponent } from './compoent/carousel/carousel.component';
+import { IndextapsComponent } from './compoent/indextaps/indextaps.component';
+import { FooterComponent } from './nav/footer/footer.component';
+import { BookdetailsComponent } from './compoent/bookdetails/bookdetails.component';
+import { CommentComponent } from './compoent/comment/comment.component';
+
+// import * as $ from '@types/jquery/dist/jquery.slim';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EditorComponent,
+    CarouselComponent,
+    IndextapsComponent,
+    FooterComponent,
+    BookdetailsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
